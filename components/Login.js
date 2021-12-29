1d96195e2a2141f1a2180e260858f64e756af85b
shreyansh018 <shreyansh.singhrajput2019@gmail.com>
2021-12-29 21:42:04 +0530

    starting build

---
+++ components/Login.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function Login() {
+  return (
+    <div className="bg-black relative text-white">
+      <h1>I am Login Screen</h1>
+      <div>
+        {/*Logo*/}
+
+        {/*Login button*/}
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Login;
